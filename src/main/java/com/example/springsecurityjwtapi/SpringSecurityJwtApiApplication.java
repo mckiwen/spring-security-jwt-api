7f3965e58248ed86248858fd111ee0918fe1e36c
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-06 13:12:52 +0000

    First Commit

---
+++ src/main/java/com/example/springsecurityjwtapi/SpringSecurityJwtApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityjwtapi/SpringSecurityJwtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
