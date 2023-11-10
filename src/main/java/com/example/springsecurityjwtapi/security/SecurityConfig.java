d01f44131592d5914bf0a3c5bb88ebd4b950a7d1
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-10 13:23:10 +0000

    Web and JWT Postman Authentication

--- src/main/java/com/example/springsecurityjwtapi/security/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjwtapi/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
                 //.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers( "/api/auth/**").permitAll()
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        //.requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults())
