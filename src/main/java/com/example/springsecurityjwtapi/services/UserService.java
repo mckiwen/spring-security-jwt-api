7e865bac71ddadc84fc12e896dd81af0135244ad
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-15 14:30:38 +0000

    + UserService

--- src/main/java/com/example/springsecurityjwtapi/services/UserService.java
+++ src/main/java/com/example/springsecurityjwtapi/services/UserService.java
@@ -1,4 +1,13 @@
 package com.example.springsecurityjwtapi.services;
 
+import com.example.springsecurityjwtapi.dtos.RegisterDTO;
+import com.example.springsecurityjwtapi.entities.UserEntity;
+
+import java.util.Optional;
+
 public interface UserService {
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
+
+    void save(RegisterDTO registerDTO);
 }
---
+++ src/main/java/com/example/springsecurityjwtapi/services/UserServiceImpl.java
@@ -0,0 +1,75 @@
+package com.example.springsecurityjwtapi.services;
+
+import com.example.springsecurityjwtapi.dtos.RegisterDTO;
+import com.example.springsecurityjwtapi.entities.Role;
+import com.example.springsecurityjwtapi.entities.UserEntity;
+import com.example.springsecurityjwtapi.repositories.RoleRepository;
+import com.example.springsecurityjwtapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserDetailsService, UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    /**
+     * This method takes a list of Roles and map it into a Collection of GrantedAuthority
+     * to define roles and permissions of users then.
+     * @param roles
+     * @return
+     */
+    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public Optional<UserEntity> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public Boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public void save(RegisterDTO registerDTO) {
+        UserEntity user = new UserEntity();
+        user.setUsername(registerDTO.getUsername());
+        user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
+
+        Role roles = roleRepository.findByName("USER").get();
+        user.setRoles(Collections.singletonList(roles));
+
+        userRepository.save(user);
+    }
+
+}
