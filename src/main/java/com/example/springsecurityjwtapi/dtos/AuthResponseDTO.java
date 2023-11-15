3de40785bb480295d9e059db017341ef87e521ee
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-15 17:35:57 +0000

    + tokenType update

--- src/main/java/com/example/springsecurityjwtapi/dtos/AuthResponseDTO.java
+++ src/main/java/com/example/springsecurityjwtapi/dtos/AuthResponseDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class AuthResponseDTO {
     private String accessToken;
-    private String tokenType = "Bearer ";
+    private String tokenType = "Bearer";
 
     public AuthResponseDTO(String accessToken) {
         this.accessToken = accessToken;
