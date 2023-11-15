61d4125b8ba507d4c87e24d69ad1bb7c13b2569c
Alberto Prieto Antúnez <albantunez91@gmail.com>
2023-11-15 19:37:08 +0000

    + NoArgsConstructor

--- src/main/java/com/example/springsecurityjwtapi/dtos/AuthResponseDTO.java
+++ src/main/java/com/example/springsecurityjwtapi/dtos/AuthResponseDTO.java
@@ -1,8 +1,10 @@
 package com.example.springsecurityjwtapi.dtos;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class AuthResponseDTO {
     private String accessToken;
     private String tokenType = "Bearer";
